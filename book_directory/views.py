f1c3630135ac8f22b30fcc4b03e6cd9208c5e4f6
AngelD-F <angeldfuenmayor21@gmail.com>
2022-04-19 03:35:08 -0400

    Pagination

--- book_directory/views.py
+++ book_directory/views.py
@@ -1,24 +1,47 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
-from book_directory.models import Book
+from django.db.models.functions import Lower
 
 from .models import Book
 from .forms import BookForm
 from .filters import BookFilter
 
-# Create your views here.
+class PaginationContext:
+    def __init__(self, items, page=1, items_per_page=10):
+        self.items = items
+        self.page = page
+        self.items_per_page = items_per_page
+        self.previous = self.page - 1
+        self.is_previous = self.previous >= 1 
+        self.next = self.page + 1
+        self.is_next = self.items_per_page*self.page < self.items.count() 
+        self.pages = range(1, self.items.count() % self.items_per_page)
+    
+    def get_items(self):
+        return self.items[self.items_per_page*(self.page-1) : self.items_per_page*self.page]
+
+
 def index(request):
     # The books most be ordered by the book number by default
-    # The first five
-    books = Book.objects.all().order_by('book_number')
+    orderby = request.GET.get('orderby', 'book_number')
+    page = int(request.GET.get('p', 1))
 
-    bookFilter = BookFilter(request.GET, queryset=Book.objects.all())
-    paginationNeeded = True
+    # Process data
+    books = Book.objects.all()
+    book_count = books.count()
+    bookFilter = BookFilter(request.GET, queryset=books)
+
+    books = bookFilter.qs
+    books = books.order_by(Lower(orderby))
+
+    # Pagination
+    pagination = PaginationContext(books, page)
+    books = pagination.get_items()
 
     ctx = {
         'books': books,
+        'book_count': book_count,
         'bookFilter': bookFilter,
-        'paginationNeeded': paginationNeeded
+        'pagination': pagination,
     }
 
     return render(request, 'book_directory/index.html', ctx)
