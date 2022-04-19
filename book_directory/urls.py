9f240c9eafab554472a1ecaec9df6925281b33c7
AngelD-F <angeldfuenmayor21@gmail.com>
2022-04-19 01:35:36 -0400

    CRUD system

--- book_directory/urls.py
+++ book_directory/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "book_directory"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addbook/', views.add_book, name='add_book'),
+    path('updatebook/<str:pk>/', views.update_book, name='update_book'),
+    path('deletebook/<str:pk>/', views.delete_book, name='delete_book'),
 ]
--- book_directory/views.py
+++ book_directory/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from book_directory.models import Book
 
 from .models import Book
+from .forms import BookForm
 from .filters import BookFilter
 
 # Create your views here.
@@ -22,3 +23,47 @@ def index(request):
 
     return render(request, 'book_directory/index.html', ctx)
 
+
+def add_book(request):
+    form = BookForm()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('../')
+
+    ctx = {
+        'form': form
+    }
+    return render(request, 'book_directory/book_form.html', ctx)
+
+
+def update_book(request, pk):
+    book = get_object_or_404(Book, id=pk)
+    form = BookForm(instance=book)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('../../')
+
+    ctx = {
+        'form': form,
+        'book': book,
+    }
+    return render(request, 'book_directory/book_form.html', ctx)
+
+
+def delete_book(request, pk):
+    book = get_object_or_404(Book, id=pk)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('../../')
+
+    ctx = {
+        'book': book,
+    }
+    return render(request, 'book_directory/delete_book.html', ctx)
